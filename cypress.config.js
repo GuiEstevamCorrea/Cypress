fb1d1287f349b97bbd3523a4fe77ac3b3fdeb50e
GuiEstevamCorrea <guilhermecorrea.2002@hotmail.com>
2023-08-03 09:31:11 -0300

    Testes criados

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'w7n65n',
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 10000,
